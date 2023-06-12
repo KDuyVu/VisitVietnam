26f9d1c8d033d52b3ea7c2b1840c196efb8eca1d
Duy Vu <kduy.vu@gmail.com>
2023-06-12 11:41:16 -0500

    VVN-69: Update weather for individual city page

--- src/app/CityPage/weather-component/Weather.component.ts
+++ src/app/CityPage/weather-component/Weather.component.ts
@@ -1,18 +1,65 @@
-import { Component, Input } from "@angular/core";
-import { City } from "src/app/service/CityService.service";
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { City } from 'src/app/service/CityService.service';
 
 @Component({
-    selector: "app-weather-component",
-    templateUrl: "./Weather.component.html",
-    styleUrls: ["./Weather.component.css"]
+  selector: 'app-weather-component',
+  templateUrl: './Weather.component.html',
+  styleUrls: ['./Weather.component.css'],
 })
-export class WeatherComponent {
-    myScriptElement: HTMLScriptElement;
+export class WeatherComponent implements OnChanges {
+  @Input() city: City = null;
+  myScriptElement: HTMLScriptElement;
 
-    constructor() {
-        this.myScriptElement = document.createElement("script");
-        this.myScriptElement.src = 'https://app1.weatherwidget.org/js/?id=ww_757c2bc9d43ee';
-        this.myScriptElement.async = true;
-        document.body.appendChild(this.myScriptElement);
+  constructor() {
+    this.myScriptElement = document.createElement('script');
+    this.myScriptElement.src =
+      'https://app1.weatherwidget.org/js/?id=ww_757c2bc9d43ee';
+    this.myScriptElement.async = true;
+    document.body.appendChild(this.myScriptElement);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("cahnged " ,this.city);
+  }
+
+  getA(): string {
+    if (!this.city) {
+      return null;
     }
+    console.log(`{
+      t: 'horizontal',
+      lang: 'en',
+      sl_lpl: 1,
+      ids: ['${this.city.weatherId}'],
+      font: 'Arial',
+      sl_ics: 'one_a',
+      sl_sot: 'fahrenheit',
+      cl_bkg: '#FFFFFF00',
+      cl_cloud: '#949494ff',
+      cl_persp: '#30b6f3ff',
+      cl_sun: '#FFC107FF',
+      cl_moon: '#FFC107FF',
+      cl_thund: '#FF5722',
+      cl_font: '#000000',
+      width: '30vw',
+    }`);
+    return `{
+      "t":"horizontal",
+      "lang":"en",
+      "sl_lpl":1,
+      "ids":["${this.city.weatherId}"],
+      "font":"Arial",
+      "sl_ics":"one_a",
+      "sl_sot":"celcius",
+      "cl_bkg":"#FFFFFF00",
+      "cl_font":"#FFFFFF",
+      "cl_cloud":"#949494ff",
+      "cl_persp":"#30b6f3ff",
+      "cl_sun":"#FFC107FF",
+      "cl_moon":"#FFC107FF",
+      "cl_thund":"#FF5722",
+      "cl_font":"#000000",
+      "width":"30vw"
+    }`;
+  }
 }
