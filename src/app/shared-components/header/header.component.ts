d59b7788317419ad19093d60cf4bb145e6cbc98c
Harry <thanhtuan@pc-199-133.customer.ask4.lan>
2023-06-21 21:02:59 +0100

    VVN-63: Add image gallery

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -8,5 +8,13 @@ import { CityService } from "src/app/service/CityService.service";
   styleUrls: ['./header.component.css']
 })
 export class SharedHeaderComponent {
-  //your code goes here
+  menuVisible: boolean = false;
+
+  showMenu() {
+    this.menuVisible = true;
+  }
+
+  hideMenu() {
+    this.menuVisible = false;
+  }
 }
