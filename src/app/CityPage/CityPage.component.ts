d14e0324124304520f8f0241f6816bf020cf5e37
Duy Vu <kduy.vu@gmail.com>
2023-07-08 14:41:48 -0500

    VVN-80: new version of VVN

--- src/app/CityPage/CityPage.component.ts
+++ src/app/CityPage/CityPage.component.ts
@@ -1,46 +1,57 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { combineLatest, switchMap } from "rxjs";
-import { City, CityExperience, CityService, SampleItinerary } from "../service/CityService.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { combineLatest, of, switchMap } from 'rxjs';
+import {
+  City,
+  CityExperience,
+  CityService,
+  SampleItinerary,
+} from '../service/CityService.service';
 
 @Component({
-    selector: "app-city-page",
-    templateUrl: "CityPage.component.html",
-    styleUrls: ["CityPage.component.css"]
+  selector: 'app-city-page',
+  templateUrl: 'CityPage.component.html',
+  styleUrls: ['CityPage.component.css'],
 })
-export class CityPageComponent implements OnInit{
-    myScriptElement: HTMLScriptElement;
-    cityId: number = null;
-    city: City = null;
-    cityExperiences: CityExperience[] = null;
-    itineraries: SampleItinerary[] = [];
+export class CityPageComponent implements OnInit {
+  myScriptElement: HTMLScriptElement;
+  cityId: number = null;
+  city: City = null;
+  cityExperiences: CityExperience[] = null;
+  itineraries: SampleItinerary[] = [];
 
-    constructor(
-        private route: ActivatedRoute,
-        private cityService: CityService,
-    ) {
-        this.myScriptElement = document.createElement("script");
-        this.myScriptElement.src = '//embedr.flickr.com/assets/client-code.js';
-        this.myScriptElement.async = true;
-        document.body.appendChild(this.myScriptElement);
-    }
+  constructor(private route: ActivatedRoute, private cityService: CityService) {
+    this.myScriptElement = document.createElement('script');
+    this.myScriptElement.src = '//embedr.flickr.com/assets/client-code.js';
+    this.myScriptElement.async = true;
+    document.body.appendChild(this.myScriptElement);
+  }
 
-    ngOnInit() {
-        this.cityId = Number(this.route.snapshot.paramMap.get('id'));
-        combineLatest([this.route.paramMap, this.cityService.cityCacheDataSource$, this.cityService.cityExperienceCacheSource$])
-            .subscribe(([params, cityCache, cityExperienceCache]) => {
-                if (!params || !cityCache || !cityExperienceCache) {
-                    return;
-                }
-                this.city = cityCache.get(this.cityId);
-                this.cityExperiences = Array.from(cityExperienceCache.values()).filter(experience => experience[0].cityId === this.cityId)[0];
-            }
-        );
+  ngOnInit() {
+    // this.cityId = Number(this.route.snapshot.paramMap.get('id'));
+    // this.route.paramMap.pipe(
+    //   switchMap(params => {
+    //     console.log("cahnged : ");
+    //     this.cityId = Number(params.get('id'));
+    //     return of(this.cityId);
+    //   })
+    // )
 
-        this.cityService.getItinerariesByCityId(this.cityId).subscribe(
-            (itineraries) => {
-                this.itineraries = itineraries;
-            }
-        )
-    }
+    combineLatest([
+      this.route.paramMap,
+      this.cityService.cityCacheDataSource$,
+      this.cityService.cityExperienceCacheSource$,
+      this.cityService.itinerariesByCityId$,
+    ]).subscribe(([params, cityCache, cityExperienceCache, itinerariesByCityIdCache]) => {
+      if (!params || !cityCache || !cityExperienceCache || !itinerariesByCityIdCache) {
+        return;
+      }
+      this.cityId = Number(params.get('id'));
+      this.city = cityCache.get(this.cityId);
+      this.cityExperiences = Array.from(cityExperienceCache.values()).filter(
+        (experience) => experience[0].cityId === this.cityId
+      )[0];
+      this.itineraries = itinerariesByCityIdCache.get(this.cityId);
+    });
+  }
 }
